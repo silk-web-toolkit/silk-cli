e719bb836174c2490761d7ebac06e06e9ea96ebc
rossputin <ross@bheap.co.uk>
2013-02-22 09:20:55 +0000

    implement full draft of basic template wrapping of views

--- src/silk/eden/cli.clj
+++ src/silk/eden/cli.clj
@@ -1,13 +1,36 @@
 (ns silk.eden.cli
   (:require [clojure.java.io :refer [file]]
-            [silk.input.env :as se])
+            [me.raynes.laser :as l]
+            [silk.input.env :as se]
+            [silk.input.file :as sf])
+  (import java.io.File)
   (:gen-class))
 
-(defn- get-templates [] (file-seq (file se/templates-path)))
+;; =============================================================================
+;; Helper functions
+;; =============================================================================
 
-(defn- get-views [] (file-seq (file se/views-path)))
+(defn- get-views [] (rest (file-seq (file se/views-path))))
+
+(defn- template-wrap
+  [v]
+  (let [parsed-view (l/parse v)]
+    {:file (.getName v)
+     :content (l/document 
+                (l/parse (sf/template "default.html"))
+                (l/id="silk-view") 
+                  (l/replace 
+                    (l/select parsed-view 
+                      (l/child-of (l/element= :body) (l/any)))))}))
+
+
+;; =============================================================================
+;; Application entry point
+;; =============================================================================
 
 (defn -main [& args]
-  (println args)
-  (println (get-templates))
-  (println (get-views)))
+  (let [views (get-views)
+        templated (map #(template-wrap %) views)]
+    (.mkdir (new File "site"))
+    (doseq [t templated]
+      (spit (str se/site-path (:file t)) (:content t)))))
