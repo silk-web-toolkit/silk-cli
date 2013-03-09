31843ef80f20ee6ce9534170063c5f3f43b25382
rossputin <ross@bheap.co.uk>
2013-03-09 22:51:22 +0000

    full draft of component injection

--- src/silk/eden/cli.clj
+++ src/silk/eden/cli.clj
@@ -3,6 +3,7 @@
             [me.raynes.laser :as l]
             [silk.input.env :as se]
             [silk.input.file :as sf])
+  (:use [clojure.string :only [split]])
   (import java.io.File)
   (:gen-class))
 
@@ -10,18 +11,44 @@
 ;; Helper functions
 ;; =============================================================================
 
+(def c-state (atom nil))
+
 (defn- get-views [] (rest (file-seq (file se/views-path))))
 
-(defn- template-wrap
+(defn- build-component
+  [i]
+  (let [comp-str (str ((split i #":") 1) ".html")
+        parsed-comp (l/parse (sf/component comp-str))]
+    (l/select parsed-comp
+              (l/child-of (l/element= :body) (l/any)))))
+
+(defn- view-inject
   [v]
   (let [parsed-view (l/parse v)]
     {:file (.getName v)
-     :content (l/document 
+     :content (l/document
                 (l/parse (sf/template "default.html"))
-                (l/id="silk-view") 
-                  (l/replace 
-                    (l/select parsed-view 
+                (l/id="silk-view")
+                  (l/replace
+                    (l/select parsed-view
                       (l/child-of (l/element= :body) (l/any)))))}))
+
+(defn- component-inject
+  [i]
+   (let [injected (l/document
+                  (l/parse @c-state)
+                  (l/id= i)
+                  (l/replace (build-component i)))]
+    (reset! c-state injected)))
+
+(defn- process-components
+  [t]
+  (let [comps (l/select (l/parse (:content t)) (l/re-id #"silk-component"))
+        comp-ids (map #(:id (:attrs %)) comps)]
+    (reset! c-state (:content t))
+    (doseq [id comp-ids]
+      (component-inject id))
+    (assoc t :content @c-state)))
 
 
 ;; =============================================================================
@@ -30,7 +57,8 @@
 
 (defn -main [& args]
   (let [views (get-views)
-        templated (map #(template-wrap %) views)]
+        templated-views (map #(view-inject %) views)
+        pages (map #(process-components %) templated-views)]
     (.mkdir (new File "site"))
-    (doseq [t templated]
+    (doseq [t pages]
       (spit (str se/site-path (:file t)) (:content t)))))
