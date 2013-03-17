067dafc2b2d86d36e026819310ee742653ddda4e
rossputin <ross@bheap.co.uk>
2013-03-17 13:13:49 +0000

    fix relativisation for Windows, add some cli feedback

--- src/silk/eden/cli.clj
+++ src/silk/eden/cli.clj
@@ -30,19 +30,30 @@
 (defn- build-component
   [i]
   (let [comp-str (str ((split i #":") 1) ".html")
-        parsed-comp (l/parse (sf/component comp-str))]
+        lcp (str se/pwd se/fs "components" se/fs comp-str)
+        c-path (if (.exists (File. lcp)) (file lcp) (sf/component comp-str))
+        parsed-comp (l/parse c-path)]
     (l/select parsed-comp
               (l/child-of (l/element= :body) (l/any)))))
+
+(defn- relativise
+  "Relativise two paths, prepping Windows paths."
+  [a b]
+  (let [doze? (if (re-find #"indow" (System/getProperty "os.name")) true false)
+        pa (if doze? (.replaceAll ((split a #":") 1) "\\\\" "/") a)
+        pb (if doze? (.replaceAll ((split b #":") 1) "\\\\" "/") b)]
+    (path/relativize pa pb)))
 
 (defn- view-inject
   [v]
   (let [parsed-view (l/parse v)
-        meta-template (l/select parsed-view 
+        meta-template (l/select parsed-view
                        (l/and (l/element= :meta) (l/attr= :name "template")))
-        template (if-not (nil? (first meta-template)) 
-                   (sf/template (str (:content (:attrs (first meta-template))) ".html"))
+        template (if-not (nil? (first meta-template))
+                   (sf/template
+                    (str (:content (:attrs (first meta-template))) ".html"))
                    (sf/template "default.html"))]
-    {:file (path/relativize se/views-path (.getPath v))
+    {:file (relativise se/views-path (.getPath v))
      :content (l/document
                 (l/parse template)
                 (l/id="silk-view")
@@ -75,9 +86,9 @@
             parsed-uri (some #{"http:" "https:" "mailto:"} pv)]
         (if-not (nil? parsed-uri)
           v
-          (let [rel (path/relativize 
-                      (.getParent (File. se/views-path p))
-                      se/views-path)]
+          (let [rel (relativise
+                     (.getParent (File. se/views-path p))
+                     se/views-path)]
             (str rel "/" v))))
       v)))
 
@@ -85,7 +96,7 @@
   [e a p]
   (let [page (l/parse (:content p))
         uri-tx (l/document
-                 page 
+                 page
                  (l/and (l/element= e) (l/attr? a))
                    (l/update-attr a relativise-attr (:file p)))]
     (assoc p :content uri-tx)))
@@ -101,11 +112,14 @@
         pages (map #(process-components %) templated-views)
         href-rewritten (map #(attrib-rewrite :link :href %) pages)
         src-rewritten (map #(attrib-rewrite :img :src %) href-rewritten)]
+    (println "Silk v0.2.0-SNAPSHOT")
     (when (.exists (File. "site")) (delete-directory "site"))
     (.mkdir (new File "site"))
     (copy-recursive "resource" "site")
     (copy-recursive "meta" "site")
+    (println "Spinning your site...")
     (doseq [t src-rewritten]
       (let [parent (.getParent (new File (:file t)))]
         (when-not (nil? parent) (.mkdirs (File. "site" parent)))
-        (spit (str se/site-path (:file t)) (:content t))))))
+        (spit (str se/site-path (:file t)) (:content t))))
+    (println "Site spinning is complete, we hope you like it.")))
