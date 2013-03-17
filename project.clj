15b2eef502b000e45cc28b1fbc6b97922e2acd38
rossputin <ross@bheap.co.uk>
2013-03-17 15:18:32 +0000

    pin silk version to 0.2.0-pre.1 and bump eden version to 0.2.0-pre.1

--- project.clj
+++ project.clj
@@ -1,10 +1,10 @@
-(defproject silk-eden "0.2.0-SNAPSHOT"
+(defproject silk-eden "0.2.0-pre.1"
   :description "Silk compile time, habitat for templates, components, inputs and transformations."
   :min-lein-version "2.0.0"
   :dependencies [[org.clojure/clojure "1.4.0"]
                  [me.raynes/laser "0.1.22"]
                  [pathetic "0.4.0"]
-                 [silk "0.2.0-SNAPSHOT"]]
+                 [silk "0.2.0-pre.1"]]
 
   :aot :all
   :main silk.eden.cli)
