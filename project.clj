69e11a00ff933021db3cac230bb67dd32408b90d
rossputin <rossajmcd@gmail.com>
2013-04-04 20:41:42 +0100

    bump version to 0.2.0-pre.3

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject silk-eden "0.2.0-pre.2"
+(defproject silk-eden "0.2.0-pre.3"
   :description "Silk compile time command line interface."
   :min-lein-version "2.0.0"
   :dependencies [[org.clojure/clojure "1.4.0"]
--- src/silk/eden/io.clj
+++ src/silk/eden/io.clj
@@ -13,7 +13,7 @@
   (println "(_-< | | / /")
   (println "/__/_|_|_\\_\\")
   (println "")
-  (println "v0.2.0-pre.2"))
+  (println "v0.2.0-pre.3"))
 
 (defn side-effecting-spin-io
   []
