2f2ad5ab3794618f9818e2978103a0b6cb9c6f9d
rossputin <rossajmcd@gmail.com>
2013-04-14 20:13:06 +0100

    bump clojure dependency to 1.5.1

--- project.clj
+++ project.clj
@@ -1,7 +1,7 @@
 (defproject silk-eden "0.2.0-pre.3"
   :description "Silk compile time command line interface."
   :min-lein-version "2.0.0"
-  :dependencies [[org.clojure/clojure "1.4.0"]
+  :dependencies [[org.clojure/clojure "1.5.1"]
                  [me.raynes/laser "0.1.22"]
                  [me.rossputin/diskops "0.1.0"]
                  [pathetic "0.4.0"]
