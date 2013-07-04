9a8d5cae3f119710211e0bddf79ebf2f43c0051b
rossputin <rossajmcd@gmail.com>
2013-07-04 22:00:37 +0100

    use new prototype silk 0.3.0

--- project.clj
+++ project.clj
@@ -6,7 +6,7 @@
                  [me.rossputin/diskops "0.1.0"]
                  [pathetic "0.4.0"]
                  [watchtower/watchtower "0.1.1"]
-                 [silk "0.2.1"]]
+                 [silk "0.3.0"]]
 
   :aot :all
   :main silk.eden.cli)
