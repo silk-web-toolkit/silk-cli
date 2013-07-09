b1a1ac33d6d9a901d54d570ff2edb559f2521637
Ross McDonald <Ross@rizzo.speed-trap.com>
2013-07-09 16:33:02 +0100

    bump version to development 0.3.0

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject silk-eden "0.2.2"
+(defproject silk-eden "0.3.0"
   :description "Silk compile time command line interface."
   :min-lein-version "2.0.0"
   :dependencies [[org.clojure/clojure "1.5.1"]
