69697d620142c8daa87bb2252c0d712bfd634983
rossputin <rossajmcd@gmail.com>
2013-07-10 22:01:10 +0100

    set to development version 0.3.0-pre.1 and update silk dependency

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject silk-eden "0.3.0"
+(defproject silk-eden "0.3.0-pre.1"
   :description "Silk compile time command line interface."
   :min-lein-version "2.0.0"
   :dependencies [[org.clojure/clojure "1.5.1"]
@@ -6,7 +6,7 @@
                  [me.rossputin/diskops "0.1.0"]
                  [pathetic "0.4.0"]
                  [watchtower/watchtower "0.1.1"]
-                 [silk "0.3.0"]]
+                 [silk "0.3.0-pre.1"]]
 
   :aot :all
   :main silk.eden.cli)
