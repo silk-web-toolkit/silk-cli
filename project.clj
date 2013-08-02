5048e3a837ece018e7b9708bd9fc0e0889a0d0d2
Ross McDonald <Ross@rizzo.speed-trap.com>
2013-08-02 10:26:35 +0100

    bump version and update silk-core dependency for 0.3.0-pre.1+20130801 release

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject silk "0.3.0-pre.1"
+(defproject silk "0.3.0-pre.1+20130801"
   :description "Silk compile time command line interface."
   :min-lein-version "2.0.0"
   :dependencies [[org.clojure/clojure "1.5.1"]
@@ -6,7 +6,7 @@
                  [me.rossputin/diskops "0.1.0"]
                  [pathetic "0.4.0"]
                  [watchtower/watchtower "0.1.1"]
-                 [silk-core "0.3.0-pre.1"]]
+                 [silk-core "0.3.0-pre.1+20130801"]]
 
   :aot :all
   :main silk.eden.cli)
