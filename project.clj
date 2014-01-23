d737c077a2aea3dc3497645be52407d325f3a5db
rossputin <rossajmcd@gmail.com>
2014-01-23 13:06:33 +0000

    bump version with build metadata

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject silk "0.4.0-beta.1"
+(defproject silk "0.4.0-beta.1+1"
   :description "Silk compile time command line interface."
   :min-lein-version "2.0.0"
   :dependencies [[org.clojure/clojure "1.5.1"]
